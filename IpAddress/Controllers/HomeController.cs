636977d6cbfa8b26415b22dfd543a633f6bb7715
Sathish Krishnamoorthi <sathishkrishna.k@gmail.com>
2019-09-06 01:53:50 -0400

    Changes to pull ipaddress

--- IpAddress/Controllers/HomeController.cs
+++ IpAddress/Controllers/HomeController.cs
@@ -5,13 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IpAddress.Models;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.IO;
 
 namespace IpAddress.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
+            var ipaddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
+            var url = string.Format("http://api.ipstack.com/{0}?access_key={1}", ipaddress, "888a0e378148f270341506f198009bce");
+            WebRequest request = WebRequest.Create(url);
+            WebResponse webResponse = request.GetResponse();
+            var responseStream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(responseStream);
+            var response = streamReader.ReadToEnd();
+            ViewData["Response"] = response;
             return View();
         }
 
--- IpAddress/Startup.cs
+++ IpAddress/Startup.cs
@@ -33,6 +33,7 @@ namespace IpAddress
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
